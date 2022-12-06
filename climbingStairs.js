07709f1a5d6def666598b202cfeb4bbb7b4e6def
tprazniewski <104758997+tprazniewski@users.noreply.github.com>
2022-12-06 21:13:45 +0100

    climbing stairs

---
+++ climbingStairs.js
@@ -0,0 +1,18 @@
+const climbingStais = (n) => {
+  if (n === 1) {
+    return n;
+  }
+  if (n === 2) {
+    return n;
+  }
+  let curr = 0;
+  let n_1 = 2;
+  let n_2 = 1;
+  for (let i = 3; i <= n; i++) {
+    curr = n_1 + n_2;
+    n_2 = n_1;
+    n_1 = curr;
+  }
+
+  return curr;
+};
