2edda57862f26305a359506fa9a4a939ad61649b
tprazniewski <104758997+tprazniewski@users.noreply.github.com>
2022-12-07 13:32:48 +0100

    add memo to recursive

--- climbingStairs.js
+++ climbingStairs.js
@@ -18,9 +18,20 @@ const climbingStais = (n) => {
 };
 
 function climbingStaisRecu(num) {
-  if (num <= 2) {
+  if (num < 2) {
     return num;
   } else {
     return climbingStaisRecu(num - 1) + climbingStaisRecu(num - 2);
   }
 }
+
+const climbingStaisMemo = (n, memo = {}) => {
+  console.log(memo);
+  if (n in memo) return memo[n];
+  if (n <= 2) return 1;
+
+  memo[n] = climbingStaisMemo(n - 1, memo) + climbingStaisMemo(n - 2, memo);
+  return memo[n];
+};
+
+console.log(climbingStaisMemo(8));
