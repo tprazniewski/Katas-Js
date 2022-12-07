e79f73037f8a78e7e4376820216e886cf90296d4
tprazniewski <104758997+tprazniewski@users.noreply.github.com>
2022-12-07 12:44:53 +0100

    ClimbingStairs recurssively

--- climbingStairs.js
+++ climbingStairs.js
@@ -8,7 +8,7 @@ const climbingStais = (n) => {
   let curr = 0;
   let n_1 = 2;
   let n_2 = 1;
-  for (let i = 3; i <= n; i++) {
+  for (let i = 3; i < n; i++) {
     curr = n_1 + n_2;
     n_2 = n_1;
     n_1 = curr;
@@ -16,3 +16,11 @@ const climbingStais = (n) => {
 
   return curr;
 };
+
+function climbingStaisRecu(num) {
+  if (num <= 2) {
+    return num;
+  } else {
+    return climbingStaisRecu(num - 1) + climbingStaisRecu(num - 2);
+  }
+}
