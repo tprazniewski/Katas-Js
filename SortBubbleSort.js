11d0ca94f39fb361d82d261d3bf6e0cecd8d1579
tprazniewski <104758997+tprazniewski@users.noreply.github.com>
2022-12-12 14:40:53 +0100

    Update SortBubbleSort.js

--- SortBubbleSort.js
+++ SortBubbleSort.js
@@ -1,12 +1,17 @@
 function bubleSort(arr) {
+  let noSwaps;
   for (let i = 0; i < arr.length; i++) {
+    console.log(i);
+    noSwaps = true;
     for (let j = 0; j < arr.length - 1 - i; j++) {
-      if (arr[j] > arr[j + 1]) {
+      if (arr[j] >= arr[j + 1]) {
+        noSwaps = false;
         let temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
       }
     }
+    if (noSwaps) break;
   }
   return arr;
 }
