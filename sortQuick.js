6391227e0e1190cc7ebc415c35800b917524a8e4
tprazniewski <104758997+tprazniewski@users.noreply.github.com>
2022-12-13 20:11:35 +0100

    quickSort

--- sortQuick.js
+++ sortQuick.js
@@ -4,20 +4,29 @@ function swap(arr, i, j) {
   arr[j] = temp;
 }
 
-function pivot(arr) {
-  let start = 0;
+function pivot(arr, start = 0, end = arr.length - 1) {
   let pivot = arr[start];
   let swapIndex = start;
 
-  for (let i = start + 1; i < arr.length; i++) {
+  for (let i = start + 1; i <= end; i++) {
     if (pivot > arr[i]) {
       swapIndex++;
       swap(arr, swapIndex, i);
     }
   }
   swap(arr, start, swapIndex);
-  console.log(arr);
   return swapIndex;
 }
 
-console.log(pivot([4, 8, 2, 1, 5, 7, 6, 3]));
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    let pivotIndex = pivot(arr, left, right);
+    //left
+    quickSort(arr, left, pivotIndex - 1);
+
+    //right
+    quickSort(arr, pivotIndex + 1, right);
+  }
+  return arr;
+}
+console.log(quickSort([4, 8, 2, 1, 5, 7, 6, 3]));
