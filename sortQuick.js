01fab2a20f8fc9d1a37b14235c73a6a9ae472fd2
tprazniewski <104758997+tprazniewski@users.noreply.github.com>
2022-12-13 19:55:19 +0100

    quickSort create pivot function

--- sortQuick.js
+++ sortQuick.js
@@ -3,3 +3,21 @@ function swap(arr, i, j) {
   arr[i] = arr[j];
   arr[j] = temp;
 }
+
+function pivot(arr) {
+  let start = 0;
+  let pivot = arr[start];
+  let swapIndex = start;
+
+  for (let i = start + 1; i < arr.length; i++) {
+    if (pivot > arr[i]) {
+      swapIndex++;
+      swap(arr, swapIndex, i);
+    }
+  }
+  swap(arr, start, swapIndex);
+  console.log(arr);
+  return swapIndex;
+}
+
+console.log(pivot([4, 8, 2, 1, 5, 7, 6, 3]));
